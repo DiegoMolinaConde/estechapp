5535e5b038d03f90b9ce05f6c061c1ef4289e1a1
DiegoMolinaConde <134484958+DiegoMolinaConde@users.noreply.github.com>
2024-05-10 04:44:16 +0200

    Add files via upload

---
+++ estechapp/iOS/EstechApp/Modelos/ModeloLogin.swift
@@ -0,0 +1,20 @@
+//
+//  ModeloLogin.swift
+//  EstechApp
+//
+//  Created by Diego Molina Conde on 21/4/24.
+//
+
+import Foundation
+
+struct LoginResponse: Codable {
+    let roles: [Role]
+    let message: String
+    let token: String
+    let username: String
+}
+
+// Modelo para representar el rol de un usuario
+struct Role: Codable {
+    let authority: String
+}
